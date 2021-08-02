ca53733c70165459b20be3d6bffed29a25822aea
Nicolas Rios <Nickrios33@yahoo.com>
2021-08-01 19:14:11 -0700

    Initial Commit

---
+++ Disco App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Disco App
+//
+//  Created by Nicolas Rios on 8/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Rectangle
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Disco App/Disco_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disco_AppApp.swift
+//  Disco App
+//
+//  Created by Nicolas Rios on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Disco_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
